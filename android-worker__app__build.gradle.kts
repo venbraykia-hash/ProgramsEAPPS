6bef9639dc1cdabfb378e820684008f57c683a0e
venbraykia-hash <venbraykia@gmail.com>
2026-09-15 17:01:21 -0300

    Add androidx.core dependency for NotificationCompat support

--- android-worker__app__build.gradle.kts
+++ android-worker__app__build.gradle.kts
@@ -23,6 +23,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core:1.13.1")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
 }
